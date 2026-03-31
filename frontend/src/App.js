581e6113a039f04db9402b7cc008adfb6b35b425
Brooks Castleberry <brookscastleberry3@gmail.com>
2026-03-31 15:33:48 -0500

    Add GA tracking + page tracking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
 import Footer from "./components/Footer";
 
+// 👇 This component handles page tracking
+function PageTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag('config', 'G-7L8VKTG3WK', {
+      page_path: location.pathname,
+    });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      {/* 👇 Add this here */}
+      <PageTracker />
+
       <div className="app-wrapper">
         <main className="page-content">
           <Routes>

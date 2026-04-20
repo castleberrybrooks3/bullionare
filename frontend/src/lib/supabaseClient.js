9e4b0a8df819dc58a8af8aab2d624fc7f9cf8c3b
Brooks Castleberry <brookscastleberry3@gmail.com>
2026-04-20 00:59:50 -0500

    fix supabase env var

--- frontend/src/lib/supabaseClient.js
+++ frontend/src/lib/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEY;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+console.log("URL:", supabaseUrl);
+console.log("KEY:", supabaseKey);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
